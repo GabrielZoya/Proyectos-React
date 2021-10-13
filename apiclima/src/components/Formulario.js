4ff25874584dce2abf349b10ee882f77bbeee90d
Gabriel Zoya <zoyagabriel@gmail.com>
2021-10-13 20:34:48 -0300

    Primer commit

---
+++ apiclima/src/components/Formulario.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+const Formulario = () => {
+  const [search, setSearch] = useState({
+    ciudad: "",
+    pais: "",
+  });
+
+  const { ciudad, pais } = search;
+
+  const handleChange = (e) => {
+    setSearch({
+      ...search,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <form>
+      <div className="input-field col s12">
+        <input
+          type="text"
+          name="ciudad"
+          id="ciudad"
+          value={ciudad}
+          onChange={handleChange}
+        />
+        <label htmlFor="ciudad">Ciudad: </label>
+      </div>
+
+      <div className="input-field col s12">
+        <select name="pais" id="pais" value={pais} onChange={handleChange}>
+          <option value="">-- Seleccione un país --</option>
+          <option value="AR">Argentina</option>
+          <option value="CO">Colombia</option>
+          <option value="CR">Costa Rica</option>
+          <option value="ES">España</option>
+          <option value="US">Estados Unidos</option>
+          <option value="MX">México</option>
+          <option value="PE">Perú</option>
+        </select>
+        <label htmlFor="pais">País: </label>
+      </div>
+    </form>
+  );
+};
+
+export default Formulario;
